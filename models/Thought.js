54ac7910d8422a2e34e1ed699c3a73b224faf151
unknown <youssef.ojeil@hotmail.com>
2022-11-25 03:31:40 -0500

    setup models

--- models/Thought.js
+++ models/Thought.js
@@ -1,10 +1,33 @@
 const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
 
 const thoughtSchema = new Schema(
     {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLegnth: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true,
 
+        },
+        reactions: [reactionSchema]
+
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
     }
-)
+);
 
 
 const Thought = model('thought', thoughtSchema);
