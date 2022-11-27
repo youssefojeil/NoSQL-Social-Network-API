f31d5a93672f7a5ae6fda8c8d5cef4c868983f52
unknown <youssef.ojeil@hotmail.com>
2022-11-27 16:19:49 -0500

    populate thoughts and friends on single user get

--- controllers/userController.js
+++ controllers/userController.js
@@ -17,6 +17,9 @@ function postUser(req, res) {
 // get user by id from db
 function getUserById(req, res) {
     User.findOne({ _id: req.params.userId })
+    .select('-__v')
+    .populate('thoughts')
+    .populate('friends')
     .then((user) =>
     !user
       ? res.status(404).json({ message: 'No user with that ID' })
--- models/User.js
+++ models/User.js
@@ -13,6 +13,7 @@ const userSchema = new Schema(
             type: String,
             required: true,
             unique: true,
+            lowercase: true,
             match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
 
         },
